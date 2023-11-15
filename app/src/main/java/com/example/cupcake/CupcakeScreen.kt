d142e8cf6929b1e5ecc08f2ae29e9bd9d63d7321
Nuray <210103331@stu.sdu.edu.kz>
2023-11-15 11:19:30 +0600

    share app

--- app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -15,6 +15,8 @@
  */
 package com.example.cupcake
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -71,6 +73,20 @@ fun CupcakeAppBar(
             }
         }
     )
+}
+private fun shareOrder(context: Context, subject: String, summary: String) {
+ val intent = Intent(Intent.ACTION_SEND).apply {
+     type = "text/plain"
+     putExtra(Intent.EXTRA_SUBJECT, subject)
+     putExtra(Intent.EXTRA_TEXT, summary)
+ }
+     context.startActivity(
+         Intent.createChooser(
+             intent,
+             context.getString(R.string.new_cupcake_order)
+         )
+     )
+
 }
 private fun cancelOrderAndNavigateToStart(
     viewModel: OrderViewModel,
@@ -142,10 +158,12 @@ fun CupcakeApp(
                )
            }
            composable(route = CupcakeScreen.Summary.name) {
+               val context = LocalContext.current
                OrderSummaryScreen(
                    orderUiState = uiState,
                    onCancelButtonClicked = {cancelOrderAndNavigateToStart(viewModel, navController)},
                    onSendButtonClicked = { subject: String, summary: String ->
+                       shareOrder(context, subject = subject, summary = summary)
 
                    },
                )

06482f614ef8207ac238c2a1c860902607327b2a
Nuray <210103331@stu.sdu.edu.kz>
2023-11-15 10:56:33 +0600

    4

--- app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -15,6 +15,7 @@
  */
 package com.example.cupcake
 
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -96,21 +97,33 @@ fun CupcakeApp(
            modifier = Modifier.padding(innerPadding)
        ){
            composable(route = CupcakeScreen.Start.name){
-               StartOrderScreen(quantityOptions = DataSource.quantityOptions)
+               StartOrderScreen(quantityOptions = DataSource.quantityOptions,
+                   onNextButtonClicked = {
+                       viewModel.setQuantity(it)
+                       navController.navigate(CupcakeScreen.Flavor.name)
+                   })
            }
            composable(route = CupcakeScreen.Flavor.name){
                val context = LocalContext.current
                SelectOptionScreen(subtotal = uiState.price,
+                   onNextButtonClicked = { navController.navigate(CupcakeScreen.Pickup.name) },
+                   onCancelButtonClicked = {},
                    options = DataSource.flavors.map { id -> context.resources.getString(id) },
-                   onSelectionChanged = {viewModel.setFlavor(it)}
+
+                   onSelectionChanged = {viewModel.setFlavor(it)},
+                   modifier = Modifier.fillMaxHeight()
                )
 
            }
            composable(route = CupcakeScreen.Pickup.name){
                SelectOptionScreen(
                    subtotal = uiState.price,
+                   onNextButtonClicked = { navController.navigate(CupcakeScreen.Summary.name) },
+                   onCancelButtonClicked = {},
                    options = uiState.pickupOptions,
-                   onSelectionChanged = { viewModel.setDate(it) }
+                   onSelectionChanged = { viewModel.setDate(it),
+                   },
+                   modifier = Modifier.fillMaxHeight()
                )
            }
            composable(route = CupcakeScreen.Summary.name) {
